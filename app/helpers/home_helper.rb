833d8642105f006b9d8a598b1276278750137fc1
jpnsingh <jpnsingh86@gmail.com>
2016-02-15 19:00:34 +0530

    [2016.02.15 | JP] - Hiding access token;

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,6 +2,6 @@ require 'octokit'
 
 module HomeHelper
   def authenticate_user
-    Octokit::Client.new(:access_token => "96d38330c6fb4e930882cc26a2e77d29e68a9836")
+    Octokit::Client.new(:access_token => "****")
   end
 end
--- app/services/comments_service.rb
+++ app/services/comments_service.rb
@@ -2,7 +2,7 @@ require 'octokit'
 
 class CommentsService
   def authenticate_user
-    Octokit::Client.new(:access_token => "96d38330c6fb4e930882cc26a2e77d29e68a9836")
+    Octokit::Client.new(:access_token => "****")
   end
 end
 
