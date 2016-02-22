9f69c287ad56c28c8932aa1f40a219997cd5ad05
jpnsingh <jpnsingh86@gmail.com>
2016-02-22 17:48:25 +0530

    [2016.02.22 | JP] - Correcting apis to hit https instead of https;

--- app/assets/javascripts/angular/constants/api_constants.js
+++ app/assets/javascripts/angular/constants/api_constants.js
@@ -2,11 +2,10 @@
 
 module.exports = angular.module('pull-git-comments.constants', [])
     .constant('gitConstants', {
-            auth_url: 'http://github.com/login/oauth/authorize',
-            access_token_url: 'http://github.com/login/oauth/access_token',
-            access_token: '****',
-            repo_url: 'http://api.github.com/orgs/OnlifeHealth/repos',
-            repo_pulls_url: 'http://api.github.com/repos/:repoName/pulls',
+            auth_url: 'https://github.com/login/oauth/authorize',
+            access_token_url: 'https://github.com/login/oauth/access_token',
+            repo_url: 'https://api.github.com/orgs/OnlifeHealth/repos',
+            repo_pulls_url: 'https://api.github.com/repos/:repoName/pulls',
             pull_comments_url: '/pull/comments'
         }
     );
