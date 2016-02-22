28863b4597b24f7521d7fdf3bc4c6fb3c96c16be
jpnsingh <jpnsingh86@gmail.com>
2016-02-22 16:47:22 +0530

    [2016.02.22 | JP] - Production Ready;

--- Gemfile
+++ Gemfile
@@ -34,17 +34,17 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
-end
-
-group :development do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
 
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+end
 
+group :development, :production do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 
@@ -69,7 +69,7 @@ group :development do
   gem 'rest-client', '~> 1.8'
 end
 
-group :development do
+group :production do
   # gem 'pg'
 
   gem 'rails_12factor'
--- app/assets/javascripts/angular/services/pull_request_service.js
+++ app/assets/javascripts/angular/services/pull_request_service.js
@@ -4,9 +4,6 @@ module.exports = angular.module('pull-git-comments.services.pullRequestService',
     .service('pullRequestService', [
         '$http', 'gitConstants', 'authService',
         function ($http, gitConstants, authService) {
-
-            let self = this;
-
             return {
                 getPullsForRepo: function (repo) {
                     return $http.get(gitConstants.repo_pulls_url.replace(':repoName', repo.full_name), {
@@ -28,10 +25,10 @@ module.exports = angular.module('pull-git-comments.services.pullRequestService',
                     return this.selectedPull
                 },
                 setPullComments: function (pullComments) {
-                    self.pullComments = pullComments
+                    this.pullComments = pullComments
                 },
                 getPullComments: function () {
-                    return self.pullComments;
+                    return this.pullComments;
                 },
                 getCommentsForRepoPull: function (repo, pullId) {
                     return $http.get('/pull/comments', {params: {repo: repo, id: pullId}})
