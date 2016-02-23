315f6c28c94523391a22655991f8834f7af08661
jpnsingh <jpnsingh86@gmail.com>
2016-02-24 00:26:30 +0530

    [2016.02.23 | JP] - Generalizing css rule; Some cleanup;

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -4,9 +4,6 @@ class SignInController < ApplicationController
   CLIENT_SECRET = ENV['GH_CLIENT_SECRET']
 
   def sign_in
-    session[:signed_in] = nil
-    session[:access_token] = nil
-
     @client_id = CLIENT_ID
     @client_secret = CLIENT_SECRET
   end
