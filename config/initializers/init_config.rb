6d129c4cae5c7b76a507b5e2afd5116e0a75c71c
jpnsingh <jpnsingh86@gmail.com>
2016-02-26 13:17:10 +0530

    [2016.02.26 | JP] - Loading secrets from env vars instead of hardcoading them in repo;

--- config/initializers/init_config.rb
+++ config/initializers/init_config.rb
@@ -1,1 +1,5 @@
-GIT_CONFIG = YAML.load_file("#{Rails.root}/config/git_config.yml")[Rails.env]
+# define global configs here as follows
+
+# git global config
+git_config_yml = File.read(File.join(Rails.root, "config/git_config.yml"))
+GIT_CONFIG = YAML.load(ERB.new(git_config_yml).result)[Rails.env]
