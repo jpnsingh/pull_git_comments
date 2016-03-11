3af571ac8007ad59ef703f8a056032fa3fcc3f8e
jpnsingh <jpnsingh86@gmail.com>
2016-03-12 03:25:05 +0530

    [2016.03.12 | JP] - Test commit;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :force_expire
+  before_filter :authenticate_token
+  before_filter :authenticate_user
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def force_expire
+
+  end
+
+  def authenticate_token
+
+  end
+
+  def authenticate_user
+
+  end
+
 end
